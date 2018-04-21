f7dd6066fb5c4de81f6d815e109164521f59ffc5
task4233 <tks.m1205@gmail.com>
2018-04-22 07:53:54 +0900

    [FIX] fixed small bugs

--- makefile.sh
+++ makefile.sh
@@ -4,7 +4,7 @@
 date=$(date "+%m%d")
 ID=$(<ID.txt)
 writing_filename="kadai${date}-${ID}"
-echo "${writing_filename}.txt"
+echo "making ${writing_filename}.txt..."
 
 for i in `seq 1 $2`
 do
@@ -13,6 +13,7 @@ do
     echo "課題$1-$i" >> ${writing_filename}.txt
     echo "ソースコード" >> ${writing_filename}.txt
     cat ${filename}.c >> ${writing_filename}.txt
+    echo " " >> ${writing_filename}.txt
     echo "実行結果" >> ${writing_filename}.txt
     bash exe.sh ${filename} ${writing_filename}.txt
     echo " " >> ${writing_filename}.txt
