b83ec0995dcdfc207685b38ed06a539c5482bd23
task4233 <tks.m1205@gmail.com>
2018-04-21 19:57:36 +0900

    [FIX] fixed small bugs

--- makefile.sh
+++ makefile.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 date=$(date "+%m%d")
@@ -9,10 +10,10 @@ for i in `seq 1 $2`
 do
     filename="kadai$i"
     # echo $filename >> kadai.txt
-    echo "課題$1-$i\n" >> ${writing_filename}.txt
-    echo "ソースコード\n" >> ${writing_filename}.txt
+    echo "課題$1-$i" >> ${writing_filename}.txt
+    echo "ソースコード" >> ${writing_filename}.txt
     cat ${filename}.c >> ${writing_filename}.txt
-    echo "\n実行結果\n" >> ${writing_filename}.txt
+    echo "実行結果" >> ${writing_filename}.txt
     bash exe.sh ${filename} ${writing_filename}.txt
-    echo "\n" >> ${writing_filename}.txt
+    echo " " >> ${writing_filename}.txt
 done
